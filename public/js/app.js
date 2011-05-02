3aad7df9157d018ef296837e7a75e9ab64e99a7e
Kenan Shifflett <kenan.shifflett@gmail.com>
2011-05-02 16:27:31 +0000

    updated backbone models, views for search

--- public/js/app.js
+++ public/js/app.js
@@ -6,46 +6,7 @@ $(document).ready(function(){
     .ajaxStop(function() {
         $(this).hide();
     });
-
-  $('.selected_content_type').click(function() {
-    $('input[name="content_type"]').val('');
-    $('#search_form').submit();
-  });
-
-  $('.facet_term').click(function() {
-    $('input[name="content_type"]').val($(this).text());
-    $('#search_form').submit();
-  });
-
-  $('#search_form')
-    .bind("ajax:beforeSend", function(evt, xhr, settings){
-    })
-    .bind("ajax:success", function(evt, data, status, xhr){
-      //$('#content').html(xhr.responseText);
-      response = $.parseJSON(xhr.responseText);
-      numFound = response["response"]["numFound"];
-      docs = response["response"]["docs"];
-      highlighting = response["highlighting"];
-      facets = response["facet_counts"]["facet_fields"];
-
-      DocApp.removeAll();
-     
-      _.each(docs, function(doc){ 
-        Docs.add({doc_id: doc.id, 
-                  snippet: highlighting[doc.id]['attr_content'], //"test",
-                  content_type:doc.content_type, 
-                  attr_created:doc.attr_created}); 
-      });
-      _.each(facets, function(facet){
-        //Facets.add({name:"test"});
-      });
-       
-    })
-    .bind('ajax:complete', function(evt, xhr, status){
-    })
-    .bind("ajax:error", function(evt, xhr, status, error){
-      $('#message').html("An error has occured.  Please try again later.").show();
-    });
+  //$('#message').html("An error has occured.  Please try again later.").show();
 
   $('#index_form')
     .bind("ajax:success", function(evt, data, status, xhr){
--- public/js/doclist.js
+++ public/js/doclist.js
@@ -1,4 +1,111 @@
 $(function() {
+  window.SearchQuery = Backbone.Model.extend({
+  });
+  window.SearchView = Backbone.View.extend({
+    el: $('#search_box'),
+
+    events: {
+      "submit #search_form":  "updateQuery",
+    },
+    initialize: function() {
+      _.bindAll(this, "search", "processQueryResult");
+      this.model.bind("change:q", this.search);
+      this.model.bind("change:fq", this.search);
+    },
+
+    updateQuery: function() {
+      this.model.set({q:$('#q').val()});
+    },
+
+    search: function() {
+      var q = this.model.get("q");
+      var fq = this.model.get("fq");
+      //validate params
+      var jqxhr = $.ajax({ url: "/search",
+        type: "GET",
+        data: ({q: q, content_type: fq}),
+        });
+      jqxhr.success(this.processQueryResult);
+    },
+
+    processQueryResult: function(data) {
+      this.model.set({qr:data});
+      response = data;
+      numFound = response["response"]["numFound"];
+      docs = response["response"]["docs"];
+      highlighting = response["highlighting"];
+      facets = response["facet_counts"]["facet_fields"];
+
+      Docs.each(function(doc) { doc.clear() });
+      Filters.each(function(filter) { filter.clear() });
+
+      _.each(docs, function(doc){
+        Docs.add({doc_id: doc.id,
+                  snippet: highlighting[doc.id]['attr_content'], //"test",
+                  content_type:doc.content_type,
+                  attr_created:doc.attr_created});
+      });
+      if (facets.hasOwnProperty("content_type")) {
+        Filters.add({name:"Content Type", terms:facets.content_type});
+      }
+    },
+  });
+  window.FilterView = Backbone.View.extend({
+    el: $('#left'),
+    events: {
+      "click .facet_term":    "addFilter",
+      "click .selected_content_type":    "removeFilter",
+    },
+    initialize: function() {
+    },
+    addFilter: function(e) {
+      this.model.set({fq:$(e.toElement).text()});
+    },
+    removeFilter: function() {
+      this.model.set({fq:""});
+    },
+  });
+  window.FilterSelectedView = Backbone.View.extend({
+    template: _.template($('#filter-selected-template').html()),
+    el: $('#selected_box'),
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.bind('change:fq', this.render);
+    },
+    render: function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    },
+  });
+  query = new SearchQuery;
+  window.AppSearchView = new SearchView({model: query});
+  window.AppFilterView = new FilterView({model: query});
+  window.AppFilterSelectedView = new FilterSelectedView({model: query});
+
+  window.FilterTerm = Backbone.Model.extend({});
+  window.FilterTerms = Backbone.Collection.extend({
+    model: FilterTerm,
+  });
+  window.FilterCategory = Backbone.Model.extend({
+    initialize: function() {
+      list = new FilterTerms;
+      values = this.get('terms');
+      var len=values.length;
+      for(var i=0; i<len; i++) {
+        if (i%2==0) {
+          ft = new FilterTerm({name:values[i], count:values[i+1]});
+          list.add(ft);
+        }
+      }
+      this.set({filterTerms: list});
+    },
+    clear: function() {
+      this.view.remove();
+    }
+  });
+  window.FilterCategoryList = Backbone.Collection.extend({ model: FilterCategory });
+  window.Filters = new FilterCategoryList;
+
   window.Doc = Backbone.Model.extend({
     EMPTY: "empty",
 
@@ -14,18 +121,28 @@ $(function() {
     }
   });
 
-  window.DocList = Backbone.Collection.extend({
-    model: Doc,
-    
-    initialize: function() {
-    },
-
-  });
-
+  window.DocList = Backbone.Collection.extend({ model: Doc });
   window.Docs = new DocList;
 
+  var FilterCategoryView = Backbone.View.extend({
+    template: _.template($('#filter-template').html()),
+
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.bind('change', this.render);
+      this.model.view = this;
+    },
+
+    render: function() {
+      $(this.el).html(this.template(this.model.toJSON()));
+      return this;
+    },
+    remove: function() {
+      $(this.el).remove();
+    }
+  });
+
   var DocView = Backbone.View.extend({
-    //tagName: "li",
     template: _.template($('#doc-template').html()),
 
     initialize: function() {
@@ -36,15 +153,9 @@ $(function() {
 
     render: function() {
       $(this.el).html(this.template(this.model.toJSON()));
-      this.setContent();
       return this;
     },
 
-    setContent: function() {
-      //var content = this.model.get('doc_id');
-      //this.$('.doc_id').text(content);
-    },
-    // Remove this view from the DOM.
     remove: function() {
       $(this.el).remove();
     }
@@ -52,35 +163,24 @@ $(function() {
   });
 
   window.DocListView = Backbone.View.extend({
-    el: $("#right"),
-
+    el: $("#content"),
     initialize: function() {
-      _.bindAll(this, 'addOne', 'addAll', 'removeAll');
-      Docs.bind('add', this.addOne);
-      Docs.bind('removeAll', this.removeAll);
+      _.bindAll(this, 'addDoc', 'addFilter');
+      Docs.bind('add', this.addDoc);
+      Filters.bind('add', this.addFilter);
     },
     
-    addOne: function(doc) {
+    addDoc: function(doc) {
       var view = new DocView({model: doc});
       this.$("#search_results").append(view.render().el);
     },
 
-    addAll: function() {
-      Docs.each(this.addOne);
-    }, 
-
-    removeOne: function(doc) {
-      doc.clear();
+    addFilter: function(filter) {
+      var view = new FilterCategoryView({model: filter});
+      this.$('#filter_box').append(view.render().el);
     },
-
-    removeAll: function() {
-      Docs.each(this.removeOne);
-      return false;
-    },
-
   });
 
-  window.DocApp = new DocListView;
-
+  window.AppDocListView = new DocListView({model: query});
 });
 
